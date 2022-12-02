5597515cd6627b50a58214bf4b98d4c461fffde6
James Mitchell <jmitchell238@gmail.com>
2022-12-02 11:02:53 -0600

    Refactor Day 2 Solution

--- 2022/AdventOfCode2022/DayTwo/DayTwo.cs
+++ 2022/AdventOfCode2022/DayTwo/DayTwo.cs
@@ -1,125 +1,54 @@
 using System;
 using System.IO;
+using System.Linq;
 
 namespace AdventOfCode2022.DayTwo;
 
-public class DayTwo
+public static class DayTwo
 {
     private static readonly string[] Input = File.ReadAllLines("../../../../AdventOfCode2022/DayTwo/Day2.txt");
 
     public static void Day2()
     {
-        Console.Write("Part 1: ");
-        PartOne();
-        Console.Write("Part 2: ");
-        PartTwo();
+        Console.WriteLine($"Part 1: {PartOne()}");
+        Console.WriteLine($"Part 2: {PartTwo()} ");
     }
 
     public static int PartOne(string[]? input = null)
     {
         input ??= Input;
 
-        var score = CalculateGameScore(input);
-        
-        Console.WriteLine(score);
-
-        return score;
+        return GetScore(false, input);
     }
 
     public static int PartTwo(string[]? input = null)
     {
         input ??= Input;
 
-        var score = CalculateGameScoreCorrected(input);
-
-        Console.WriteLine(score);
-
-        return score;
+        return GetScore(true, input);
     }
 
-    private static int CalculateGameScore(string[] input)
+    private static int GetScore(bool explained, string[]? input = null)
     {
-        var sum = 0;
-        foreach (var line in input)
-        {
-            // A =   Rock   = X (1 point)
-            // B =   Paper  = Y (2 points)
-            // C = Scissors = Z (3 points)
-            
-            // WIN = 6 points
-            // DRAW = 3 points
-            // LOSS = 0 points
-            
-            string[] round = line.Split(' ');
-            var opponentChoice = round[0];
-            var playerChoice = round[1];
-
-            if (opponentChoice == "A") // ROCK
-            {
-                if (playerChoice == "X") sum += 4; // DRAW
-                if (playerChoice == "Y") sum += 8; // WIN
-                if (playerChoice == "Z") sum += 3; // LOSS
-            }
-            else if (opponentChoice == "B") // PAPER
-            {
-                if (playerChoice == "X") sum += 1; // LOSS 
-                if (playerChoice == "Y") sum += 5; // DRAW
-                if (playerChoice == "Z") sum += 9; // WIN
-            }
-            else if (opponentChoice == "C") // SCISSORS
-            {
-                if (playerChoice == "X") sum += 7; // WIN
-                if (playerChoice == "Y") sum += 2; // LOSS
-                if (playerChoice == "Z") sum += 6; // DRAW
-            }
-        }
-
-        return sum;
+        return (from line in input let round = line.Split(' ') select GetRoundScore(line, explained)).Sum();
     }
 
-    private static int CalculateGameScoreCorrected(string[] input)
+    private static int GetRoundScore(string round, bool explained)
     {
-        
-        var sum = 0;
-        foreach (var line in input)
+        (char opponent, char player) playerChoices = (round[0], round[2]);
+
+        return playerChoices switch
         {
-            // A =   Rock   = X (1 point)
-            // B =   Paper  = Y (2 points)
-            // C = Scissors = Z (3 points)
-            
-        
-            // WIN = 6 points
-            // DRAW = 3 points
-            // LOSS = 0 points
-            
-            // X = lose
-            // Y = draw
-            // Z = win
-            
-            string[] round = line.Split(' ');
-            var opponentChoice = round[0];
-            var playerChoice = round[1];
-
-            if (opponentChoice == "A") // ROCK
-            {
-                if (playerChoice == "X") sum += 3; // LOSS
-                if (playerChoice == "Y") sum += 4; // DRAW
-                if (playerChoice == "Z") sum += 8; // WIN
-            }
-            else if (opponentChoice == "B") // PAPER
-            {
-                if (playerChoice == "X") sum += 1; // LOSS 
-                if (playerChoice == "Y") sum += 5; // DRAW
-                if (playerChoice == "Z") sum += 9; // WIN
-            }
-            else if (opponentChoice == "C") // SCISSORS
-            {
-                if (playerChoice == "X") sum += 2; // LOSS
-                if (playerChoice == "Y") sum += 6; // DRAW
-                if (playerChoice == "Z") sum += 7; // WIN
-            }
-        }
-
-        return sum;
+            ('B', 'X') => 1,
+            ('B', 'Y') => 5,
+            ('B', 'Z') => 9,
+            ('A', 'X') => explained ? 3 : 4,
+            ('A', 'Y') => explained ? 4 : 8,
+            ('A', 'Z') => explained ? 8 : 3,
+            ('C', 'X') => explained ? 2 : 7,
+            ('C', 'Y') => explained ? 6 : 2,
+            ('C', 'Z') => explained ? 7 : 6,
+            _ => throw new NotImplementedException()
+        };
     }
 }
