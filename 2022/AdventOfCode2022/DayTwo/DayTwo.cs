1e51c6ab0413a6305084428d6777c071c78c0363
James Mitchell <jmitchell238@gmail.com>
2022-12-06 12:47:29 -0600

    Refactor Day 2, Add dictionaries and add tests

--- 2022/AdventOfCode2022/DayTwo/DayTwo.cs
+++ 2022/AdventOfCode2022/DayTwo/DayTwo.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -7,6 +8,20 @@ namespace AdventOfCode2022.DayTwo;
 public static class DayTwo
 {
     private static readonly string[] Input = File.ReadAllLines("../../../../AdventOfCode2022/DayTwo/Day2.txt");
+
+    private static readonly Dictionary<char, Dictionary<char, int>> MovesP1 = new()
+    {
+        {'A', new Dictionary<char, int>(){{'X', 4}, {'Y', 8}, {'Z', 3}}},
+        {'B', new Dictionary<char, int>(){{'X', 1}, {'Y', 5}, {'Z', 9}}},
+        {'C', new Dictionary<char, int>(){{'X', 7}, {'Y', 2}, {'Z', 6}}},
+    };
+
+    private static readonly Dictionary<char, Dictionary<char, int>> MovesP2 = new()
+    {
+        {'A', new Dictionary<char, int>(){{'X', 3}, {'Y', 4}, {'Z', 8}}},
+        {'B', new Dictionary<char, int>(){{'X', 1}, {'Y', 5}, {'Z', 9}}},
+        {'C', new Dictionary<char, int>(){{'X', 2}, {'Y', 6}, {'Z', 7}}},
+    };
 
     public static void Day2()
     {
@@ -28,27 +43,40 @@ public static class DayTwo
         return GetScore(true, input);
     }
 
-    private static int GetScore(bool explained, string[]? input = null)
+    public static int GetScore(bool explained, string[]? input = null)
     {
         return (from line in input let round = line.Split(' ') select GetRoundScore(line, explained)).Sum();
     }
 
-    private static int GetRoundScore(string round, bool explained)
+    public static int GetRoundScore(string round, bool explained)
     {
-        (char opponent, char player) playerChoices = (round[0], round[2]);
-
-        return playerChoices switch
+        /*
+         * Refactored to Dictionary Lookup
+         */
+        return explained switch
         {
-            ('B', 'X') => 1,
-            ('B', 'Y') => 5,
-            ('B', 'Z') => 9,
-            ('A', 'X') => explained ? 3 : 4,
-            ('A', 'Y') => explained ? 4 : 8,
-            ('A', 'Z') => explained ? 8 : 3,
-            ('C', 'X') => explained ? 2 : 7,
-            ('C', 'Y') => explained ? 6 : 2,
-            ('C', 'Z') => explained ? 7 : 6,
-            _ => throw new NotImplementedException()
+            true => MovesP2[round[0]][round[2]],
+            false => MovesP1[round[0]][round[2]]
         };
+        
+        /*
+         * Original 
+         */
+
+        // (char opponent, char player) playerChoices = (round[0], round[2]);
+
+        // return playerChoices switch
+        // {
+        //     ('B', 'X') => 1,
+        //     ('B', 'Y') => 5,
+        //     ('B', 'Z') => 9,
+        //     ('A', 'X') => explained ? 3 : 4,
+        //     ('A', 'Y') => explained ? 4 : 8,
+        //     ('A', 'Z') => explained ? 8 : 3,
+        //     ('C', 'X') => explained ? 2 : 7,
+        //     ('C', 'Y') => explained ? 6 : 2,
+        //     ('C', 'Z') => explained ? 7 : 6,
+        //     _ => throw new NotImplementedException()
+        // };
     }
 }
--- 2022/AdventOfCode2022Test/DayTwo/DayTwoTest.cs
+++ 2022/AdventOfCode2022Test/DayTwo/DayTwoTest.cs
@@ -3,13 +3,14 @@ namespace AdventOfCode2022Test.DayTwo;
 [TestClass]
 public class DayTwoTest
 {
+    private readonly string[] _input = File.ReadAllLines("../../../../AdventOfCode2022Test/DayTwo/Day2Test.txt");
+    
     [TestMethod]
     public void PartOne_Called_ReturnsCorrectAnswer()
     {
-        var input = File.ReadAllLines("../../../../AdventOfCode2022Test/DayTwo/Day2Test.txt");
         const int expect = 15;
 
-        var result = AdventOfCode2022.DayTwo.DayTwo.PartOne(input);
+        var result = AdventOfCode2022.DayTwo.DayTwo.PartOne(_input);
 
         Assert.AreEqual(expect, result);
     }
@@ -17,10 +18,51 @@ public class DayTwoTest
     [TestMethod]
     public void PartTwo_Called_ReturnsCorrectAnswer()
     {
-        var input = File.ReadAllLines("../../../../AdventOfCode2022Test/DayTwo/Day2Test.txt");
         const int expect = 12;
 
-        var result = AdventOfCode2022.DayTwo.DayTwo.PartTwo(input);
+        var result = AdventOfCode2022.DayTwo.DayTwo.PartTwo(_input);
+
+        Assert.AreEqual(expect, result);
+    }
+    
+    [TestMethod]
+    public void GetScoreNotExplained_Called_ReturnsCorrectAnswer()
+    {
+        const int expect = 15;
+
+        var result = AdventOfCode2022.DayTwo.DayTwo.GetScore(false, _input);
+
+        Assert.AreEqual(expect, result);
+    }
+    
+    [TestMethod]
+    public void GetScoreExplained_Called_ReturnsCorrectAnswer()
+    {
+        const int expect = 12;
+
+        var result = AdventOfCode2022.DayTwo.DayTwo.GetScore(true, _input);
+
+        Assert.AreEqual(expect, result);
+    }
+    
+    [TestMethod]
+    public void GetRoundScoreNotExplained_Called_ReturnsCorrectAnswer()
+    {
+        var line = _input[0];
+        const int expect = 8;
+
+        var result = AdventOfCode2022.DayTwo.DayTwo.GetRoundScore(line, false);
+
+        Assert.AreEqual(expect, result);
+    }
+
+    [TestMethod]
+    public void GetRoundScoreExplained_Called_ReturnsCorrectAnswer()
+    {
+        var line = _input[0];
+        const int expect = 4;
+
+        var result = AdventOfCode2022.DayTwo.DayTwo.GetRoundScore(line, true);
 
         Assert.AreEqual(expect, result);
     }
