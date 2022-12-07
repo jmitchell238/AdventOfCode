d39714fa4cc5a7a992f958b668909f56e9d405da
James Mitchell <jmitchell238@gmail.com>
2022-12-07 09:22:13 -0600

    Refector work

--- 2022/AdventOfCode2022/DaySeven/DaySeven.cs
+++ 2022/AdventOfCode2022/DaySeven/DaySeven.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.IO;
 using System.Linq;
-using System.Runtime.ExceptionServices;
 
 namespace AdventOfCode2022.DaySeven;
 
