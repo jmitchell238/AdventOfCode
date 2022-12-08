9ff01616ff98e6598aff1d4d0dc78604a99a2b46
James Mitchell <jmitchell238@gmail.com>
2022-12-08 15:40:27 -0600

    some refactor work

--- 2022/AdventOfCode2022/DayEight/DayEight.cs
+++ 2022/AdventOfCode2022/DayEight/DayEight.cs
@@ -19,17 +19,30 @@ public class DayEight
     {
         input ??= Input;
         
-        int visible = 0;
-        for(int i=0; i<input.Length; i++)
+        var visible = 0;
+        // Loop on the Row (top to bottom)
+        for(var i = 0; i < input.Length; i++)
         {
-            for(int j=0; j < input[i].Length;j++)
+            // Loop on the Column (left to right)
+            for(var j = 0; j < input[i].Length;j++)
             {
-                bool left = CountVisible(input, j, i, -1, 0, input[i][j]);
-                bool right= CountVisible(input, j, i, 1, 0, input[i][j]);
-                bool up = CountVisible(input, j, i, 0, -1, input[i][j]);
-                bool down= CountVisible(input, j, i, 0, 1, input[i][j]);
+                // j = y(column)
+                // i = x(row)
+                // 1/-1/0 = xd(value next to startValue on row)
+                // 1/-1/0 = yd(value next to startValue on column)
+                // input[i][j] = startValue
+                
+                // Visible from directions:
+                var left = CountVisible(input, j, i, -1, 0, input[i][j]);
+                var right= CountVisible(input, j, i, 1, 0, input[i][j]);
+                var up = CountVisible(input, j, i, 0, -1, input[i][j]);
+                var down= CountVisible(input, j, i, 0, 1, input[i][j]);
+                
+                // If it is visible from any side, add one to the count.
                 if (up || down || left || right)
+                {
                     visible++;
+                }
             }
         }
 
@@ -55,17 +68,28 @@ public class DayEight
         
         return +scenicScores.Max();
     }
-    
+
     public static bool CountVisible(string[] input, int x, int y, int xd, int yd, char startValue)
     {
-        if (x == 0 || x == input.Length - 1 || y == 0 || y == input.Length - 1)
-            return true;
-        if (startValue <= input[y + yd][x + xd])
-            return false;
+        while (true)
+        {
+            // If the startValue is on the exterior of grid, return TRUE, yes it is visible.
+            if (x == 0 || x == input.Length - 1 || y == 0 || y == input.Length - 1)
+            {
+                return true;
+            }
+            
+            // If the startValue is less or equal to than the value to left / right, return false.
+            if (startValue <= input[y + yd][x + xd])
+            {
+                return false;
+            }
 
-        return CountVisible(input, x + xd, y + yd, xd, yd, startValue);
+            x = x + xd;
+            y = y + yd;
+        }
     }
-        
+
     public static int CountScenicScore(string[] input, int x, int y, int xd, int yd, char startValue)
     {
         if (x == 0 || x == input.Length - 1 || y == 0 || y == input.Length - 1)
