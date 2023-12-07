143e8dfadacdc69dea7802db939e01658004da2d
James Mitchell <jmitchell238@gmail.com>
2023-12-06 23:18:23 -0600

    Working on day 3 part 1 - Got a grid printing out and all part number combo's being printed out. Baby Steps.

--- Java/AdventOfCodeJava/src/main/java/org/jmitchell238/aoc/aoc2023/day03/Day03.java
+++ Java/AdventOfCodeJava/src/main/java/org/jmitchell238/aoc/aoc2023/day03/Day03.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 
 public class Day03 {
 
-  private static final Boolean DEBUGGING = false;
+  private static final Boolean DEBUGGING = true;
   private static final Boolean VERBOSE = false;
 
   public static void Day03() throws FileNotFoundException {
@@ -47,8 +47,8 @@ public class Day03 {
 
       long partNumberSum = 0;
 
-      for (int y = 0 ; y < coordinateMap.size() ; y++) {
-        for (int x = 0 ; x < coordinateMap.size() ; x++) {
+      for (int y = 0 ; y < char2dArray.size() ; y++) {
+        for (int x = 0 ; x < char2dArray.get(x).length - 1 ; x++) {
           Point currentPoint = new Point(x,y);
           char currentChar = coordinateMap.get(currentPoint);
           boolean isDigit = Character.isDigit(currentChar);
@@ -56,22 +56,95 @@ public class Day03 {
           int partnumberLength = 0;
 
           if (isDigit) {
-//            partnumberLength++;
+            partnumberLength++;
             partNumber.append(currentChar);
 
-            while (Character.isDigit(coordinateMap.get(new Point(currentPoint.x + partnumberLength, currentPoint.y)))) {
-              partnumberLength++;
+            while (isDigit) {
+//              partnumberLength++;
+
               partNumber.append(coordinateMap.get(new Point(currentPoint.x + partnumberLength, currentPoint.y)));
+
+              partnumberLength++;
+
+              isDigit = Character.isDigit(coordinateMap.get(new Point(currentPoint.x + partnumberLength, currentPoint.y)));
             }
 
             if (true) {
               System.out.println("Part Number: " + partNumber);
             }
 
+
+//            if (partnumberLength > 1) {
+//              if (currentPoint.x > 0) {
+//                Point previousPoint = new Point(currentPoint.x - 1, currentPoint.y);
+//                char previousChar = coordinateMap.get(previousPoint);
+//                boolean isPreviousCharDigit = Character.isDigit(previousChar);
+//                if (isPreviousCharDigit) {
+//                  continue;
+//                }
+//              }
+//              if (currentPoint.y > 0) {
+//                Point previousPoint = new Point(currentPoint.x, currentPoint.y - 1);
+//                char previousChar = coordinateMap.get(previousPoint);
+//                boolean isPreviousCharDigit = Character.isDigit(previousChar);
+//                if (isPreviousCharDigit) {
+//                  continue;
+//                }
+//              }
+//              if (currentPoint.x > 0 && currentPoint.y > 0) {
+//                Point previousPoint = new Point(currentPoint.x - 1, currentPoint.y - 1);
+//                char previousChar = coordinateMap.get(previousPoint);
+//                boolean isPreviousCharDigit = Character.isDigit(previousChar);
+//                if (isPreviousCharDigit) {
+//                  continue;
+//                }
+//              }
+//              if (currentPoint.x > 0 && currentPoint.y < char2dArray.size() - 1) {
+//                Point previousPoint = new Point(currentPoint.x - 1, currentPoint.y + 1);
+//                char previousChar = coordinateMap.get(previousPoint);
+//                boolean isPreviousCharDigit = Character.isDigit(previousChar);
+//                if (isPreviousCharDigit) {
+//                  continue;
+//                }
+//              }
+//                if (currentPoint.y < char2dArray.size() - 1) {
+//                    Point previousPoint = new Point(currentPoint.x, currentPoint.y + 1);
+//                    char previousChar = coordinateMap.get(previousPoint);
+//                    boolean isPreviousCharDigit = Character.isDigit(previousChar);
+//                    if (isPreviousCharDigit) {
+//                    continue;
+//                    }
+//                }
+//                if (currentPoint.x < char2dArray.get(x).length - 1) {
+//                    Point previousPoint = new Point(currentPoint.x + 1, currentPoint.y);
+//                    char previousChar = coordinateMap.get(previousPoint);
+//                    boolean isPreviousCharDigit = Character.isDigit(previousChar);
+//                    if (isPreviousCharDigit) {
+//                    continue;
+//                    }
+//                }
+//                if (currentPoint.x < char2dArray.get(x).length - 1 && currentPoint.y < char2dArray.size() - 1) {
+//                    Point previousPoint = new Point(currentPoint.x + 1, currentPoint.y + 1);
+//                    char previousChar = coordinateMap.get(previousPoint);
+//                    boolean isPreviousCharDigit = Character.isDigit(previousChar);
+//                    if (isPreviousCharDigit) {
+//                    continue;
+//                    }
+//                }
+//                if (currentPoint.x < char2dArray.get(x).length - 1 && currentPoint.y > 0) {
+//                    Point previousPoint = new Point(currentPoint.x + 1, currentPoint.y - 1);
+//                    char previousChar = coordinateMap.get(previousPoint);
+//                    boolean isPreviousCharDigit = Character.isDigit(previousChar);
+//                    if (isPreviousCharDigit) {
+//                    continue;
+//                    }
+//                }
+//            }
+
             String partNumberString = partNumber.toString();
             Long partNumberLong = Long.parseLong(partNumberString);
             partNumberSum += partNumberLong;
-            x += partnumberLength;
+            x += partnumberLength - 1;
             partNumber = new StringBuilder();
             }
           }
@@ -81,6 +154,7 @@ public class Day03 {
       // iterate left to right, find "DIGIT/NUMBER"
       // Set number length to start at 1
 
+      System.out.println("Part Number Sum: " + partNumberSum);
     } catch(FileNotFoundException e) {
       // do nothing
     }
@@ -110,8 +184,8 @@ public class Day03 {
         System.out.println(character);
       }
     }
-    Point coordinate = new Point(7,2);
-    System.out.println("Character at : (7,2) = " + coordinateMap.get(coordinate));
+//    Point coordinate = new Point(7,2);
+//    System.out.println("Character at : (7,2) = " + coordinateMap.get(coordinate));
 
   }
 }
