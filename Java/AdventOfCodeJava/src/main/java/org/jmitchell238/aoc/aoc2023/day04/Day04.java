c65f53a9feba9d4b8a5fe89db1829099719a2947
James Mitchell <jmitchell238@gmail.com>
2023-12-10 09:49:48 -0600

    Complete Day 4 - Part 2

--- Java/AdventOfCodeJava/src/main/java/org/jmitchell238/aoc/aoc2023/day04/Day04.java
+++ Java/AdventOfCodeJava/src/main/java/org/jmitchell238/aoc/aoc2023/day04/Day04.java
@@ -14,11 +14,11 @@ public class Day04 {
   private static final Boolean DEBUGGING = false;
   private static final Boolean VERBOSE = false;
 
-  public static void main(String[] args) throws FileNotFoundException {
+  public void main(String[] args) throws FileNotFoundException {
     Day04Run();
   }
 
-  public static void Day04Run() {
+  public void Day04Run() {
     System.out.println("\n--- Day 4: Scratchcards ---\n");
 
     String input = "src/main/java/org/jmitchell238/aoc/aoc2023/day04/input.txt";
@@ -27,7 +27,8 @@ public class Day04 {
     int partOneAnswer = part1(input);
     System.out.println(STR."Part 1: Answer: \{partOneAnswer}");
 
-    int partTwoAnswer = part2(inputTest);
+
+    int partTwoAnswer = this.part2(input);
     System.out.println(STR."Part 2: Answer: \{partTwoAnswer}");
   }
 
@@ -52,8 +53,8 @@ public class Day04 {
       }
 
       for (ArrayList<String> line : inputSplit) {
-        ArrayList<Integer> winningNumbers = new ArrayList<>();
-        ArrayList<Integer> scratchNumbers = new ArrayList<>();
+        ArrayList<Integer> winningNumbers;
+        ArrayList<Integer> scratchNumbers;
         ArrayList<Integer> matchingNumbers = new ArrayList<>();
 
         winningNumbers = stream(line.get(1).split(" ")).filter(n -> !Objects.equals(n, "")).mapToInt(Integer::parseInt).boxed()
@@ -102,7 +103,114 @@ public class Day04 {
     }
   }
 
-  public static int part2(String inputString) {
-    return -1;
+  public int part2(String inputString) {
+    int totalCards = 0;
+
+    ArrayList<CountedArrayList> inputSplitWithCount = new ArrayList<>();
+
+    try {
+      File input = new File(inputString);
+      Scanner scanner = new Scanner(input);
+      while (scanner.hasNextLine()) {
+        String line = scanner.nextLine();
+        totalCards++;
+        ArrayList<ArrayList<String>> card = new ArrayList<>();
+        card.add(new ArrayList<>(Arrays.asList(line.split(": |\\|"))));
+        inputSplitWithCount.add(new CountedArrayList(1 , card));
+
+        if (DEBUGGING) System.out.println(line);
+      }
+      scanner.close();
+
+      for (int i = 0; i < inputSplitWithCount.size(); i++) {
+        if (DEBUGGING) System.out.println(inputSplitWithCount.get(i).getCount());
+        if (DEBUGGING) System.out.println(inputSplitWithCount.get(i).getList());
+      }
+
+      for (int i = 0; i < inputSplitWithCount.size(); i++) {
+        // I need to check how many matching numbers there are.
+        ArrayList<Integer> winningNumbers;
+        ArrayList<Integer> scratchNumbers;
+        ArrayList<Integer> matchingNumbers = new ArrayList<>();
+
+        winningNumbers = stream(inputSplitWithCount.get(i).getList().get(0).get(1).split(" ")).filter(n -> !Objects.equals(n, "")).mapToInt(Integer::parseInt).boxed()
+            .collect(toCollection(ArrayList::new));
+        if (DEBUGGING) {
+          System.out.print(STR."\n\{inputSplitWithCount.get(i).getList().get(0).getFirst()}:\n");
+          System.out.print("Winning Numbers: ");
+          for (Integer number : winningNumbers) System.out.print(STR."\{number},");
+        }
+
+        scratchNumbers = stream(inputSplitWithCount.get(i).getList().get(0).get(2).split(" ")).filter(n -> !Objects.equals(n, "")).mapToInt(Integer::parseInt).boxed()
+            .collect(toCollection(ArrayList::new));
+
+        if (DEBUGGING) {
+          System.out.print("\nScratch Numbers:");
+          for (Integer number : scratchNumbers) System.out.print(STR."\{number},");
+        }
+
+        for (Integer number : scratchNumbers) {
+          if (winningNumbers.contains(number)) {
+            matchingNumbers.add(number);
+          }
+        }
+        if (DEBUGGING) {
+          System.out.print("\nMatching Numbers:");
+          for (Integer number : matchingNumbers) System.out.print(STR."\{number},");
+        }
+
+        int extraCards = matchingNumbers.size();
+
+        for (int p = 0; p < inputSplitWithCount.size(); p++) {
+          if (DEBUGGING) System.out.println(STR."\nAmount of Cards: \{inputSplitWithCount.get(p).getCount()}");
+          if (DEBUGGING) System.out.println(inputSplitWithCount.get(p).getList());
+        }
+
+        // Loop through each card and loop through it the amount of "count"/cards it has
+        for (int cardCount = 0 ; cardCount < inputSplitWithCount.get(i).getCount() ; cardCount++) {
+          for (int j = 0; j < extraCards; j++) {
+            if (i + j + 1 < inputSplitWithCount.size()) {
+              inputSplitWithCount.get(i + j + 1).count = inputSplitWithCount.get(i + j + 1).count + 1;
+            }
+          }
+        }
+
+      }
+
+      if (DEBUGGING) System.out.println("\nAFTER WORKING ON ADDING CARDS");
+
+      for (int i = 0; i < inputSplitWithCount.size(); i++) {
+        if (DEBUGGING) System.out.println(STR."\nAmount of Cards: \{inputSplitWithCount.get(i).getCount()}");
+        if (DEBUGGING) System.out.println(inputSplitWithCount.get(i).getList());
+      }
+
+      totalCards = 0;
+
+      for (int card = 0; card < inputSplitWithCount.size(); card++) {
+        totalCards += inputSplitWithCount.get(card).getCount();
+      }
+
+      return totalCards;
+    } catch (FileNotFoundException ex) {
+      throw new RuntimeException(ex);
+    }
+  }
+
+  class CountedArrayList {
+    private int count;
+    private ArrayList<ArrayList<String>> list;
+
+    public CountedArrayList(int count, ArrayList<ArrayList<String>> list) {
+      this.count = count;
+      this.list = list;
+    }
+
+    public int getCount() {
+      return count;
+    }
+
+    public ArrayList<ArrayList<String>> getList() {
+      return list;
+    }
   }
 }
--- Java/AdventOfCodeJava/src/test/java/org/jmitchell238/aoc/aoc2023/day04/Day04Tests.java
+++ Java/AdventOfCodeJava/src/test/java/org/jmitchell238/aoc/aoc2023/day04/Day04Tests.java
@@ -1,13 +1,13 @@
 package org.jmitchell238.aoc.aoc2023.day04;
 
-import java.io.FileNotFoundException;
-import org.assertj.core.api.AssertionsForClassTypes;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 import org.junit.jupiter.api.Test;
 
 class Day04Tests {
 
   @Test
-  void Day04Part1_CalledWithTestInput_Expect13() throws FileNotFoundException {
+  void Day04Part1_CalledWithTestInput_Expect13() {
     // Arrange
     String day04Part1Input =
         "src/main/java/org/jmitchell238/aoc/aoc2023/day04/input_test.txt";
@@ -16,43 +16,47 @@ class Day04Tests {
     int expected = 13;
 
     // Act
-    AssertionsForClassTypes.assertThat(Day04.part1(day04Part1Input)).isEqualTo(expected);
+    assertThat(Day04.part1(day04Part1Input)).isEqualTo(expected);
   }
 
   @Test
-  void Day04Part1_CalledWithRealInput_ExpectCorrectAnswer() throws FileNotFoundException {
+  void Day04Part1_CalledWithRealInput_Expect15268() {
     // Arrange
     String day04Part1Input = "src/main/java/org/jmitchell238/aoc/aoc2023/day04/input.txt";
 
     // Assert
-    int expected = 0;
+    int expected = 15268;
 
     // Act
-    AssertionsForClassTypes.assertThat(Day04.part1(day04Part1Input)).isEqualTo(expected);
+    assertThat(Day04.part1(day04Part1Input)).isEqualTo(expected);
   }
 
   @Test
-  void Day04Part2_CalledWithTestInput_ExpectCorrectAnswer() throws FileNotFoundException {
+  void Day04Part2_CalledWithTestInput_Expect30() {
     // Arrange
     String day04Part2Input =
         "src/main/java/org/jmitchell238/aoc/aoc2023/day04/input_test.txt";
 
     // Assert
-    int expected = 0;
+    int expected = 30;
+    Day04 day04 = new Day04();
 
     // Act
-    AssertionsForClassTypes.assertThat(Day04.part2(day04Part2Input)).isEqualTo(expected);
+    int actual = day04.part2(day04Part2Input);
+    assertThat(actual).isEqualTo(expected);
   }
 
   @Test
-  void Day04Part2_CalledWithRealInput_ExpectCorrectAnswer() throws FileNotFoundException {
+  void Day04Part2_CalledWithRealInput_Expect6283755() {
     // Arrange
     String day04Part2Input = "src/main/java/org/jmitchell238/aoc/aoc2023/day04/input.txt";
 
     // Assert
-    int expected = 0;
+    int expected = 6283755;
+    Day04 day04 = new Day04();
 
     // Act
-    AssertionsForClassTypes.assertThat(Day04.part2(day04Part2Input)).isEqualTo(expected);
+    int actual = day04.part2(day04Part2Input);
+    assertThat(actual).isEqualTo(expected);
   }
 }
