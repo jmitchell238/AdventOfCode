b309177456aa4cc240f47e03bffab2ee72e415b4
James Mitchell <jmitchell238@gmail.com>
2023-12-13 15:19:34 -0600

    Setup for day 10

---
+++ Java/AdventOfCodeJava/src/main/java/org/jmitchell238/aoc/aoc2023/day10/Day10.java
@@ -0,0 +1,49 @@
+package org.jmitchell238.aoc.aoc2023.day10;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import lombok.Getter;
+import lombok.Setter;
+
+public class Day10 {
+  private static final Boolean DEBUGGING = true;
+  private static final Boolean VERBOSE = false;
+  @Getter @Setter
+  private Boolean isPartTwo = false;
+
+  public void main(String[] args) throws FileNotFoundException {
+    Day10Run();
+  }
+
+  public void Day10Run() throws FileNotFoundException {
+    System.out.println("\n--- Day Day 10: Pipe Maze ---\n");
+
+    String input = "src/main/java/org/jmitchell238/aoc/aoc2023/day10/input.txt";
+    String inputTest = "src/main/java/org/jmitchell238/aoc/aoc2023/day10/input_test.txt";
+
+    long partOneAnswer = part1(input);
+    System.out.println(STR."Part 1: Answer: \{partOneAnswer}");
+
+    long partTwoAnswer = part2(input);
+    System.out.println(STR."Part 2: Answer: \{partTwoAnswer}");
+  }
+
+  public long part1(String filePath) throws FileNotFoundException {
+    processFile(filePath);
+    return -1;
+  }
+
+  public long part2(String filePath) throws FileNotFoundException {
+    processFile(filePath);
+    return -1;
+  }
+
+  private void processFile(String filePath) throws FileNotFoundException {
+    Scanner scanner = new Scanner(new File(filePath));
+
+    while (scanner.hasNextLine()) {
+      String line = scanner.nextLine();
+    }
+  }
+}
---
+++ Java/AdventOfCodeJava/src/test/java/org/jmitchell238/aoc/aoc2023/day10/Day10Tests.java
@@ -0,0 +1,84 @@
+package org.jmitchell238.aoc.aoc2023.day10;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.io.FileNotFoundException;
+import org.junit.jupiter.api.Test;
+
+class Day10Tests {
+
+  @Test
+  void Day10Part1_CalledWithTestInput_Expect4() throws FileNotFoundException {
+    // Arrange
+    String day10Part1Input =
+        "src/main/java/org/jmitchell238/aoc/aoc2023/day10/input_test_1.txt";
+    Day10 day10 = new Day10();
+
+    // Assert
+    long expected = 4L;
+    long actual = day10.part1(day10Part1Input);
+
+    // Act
+    assertThat(actual).isEqualTo(expected);
+  }
+
+  @Test
+  void Day10Part1_CalledWithTestInput2_Expect8() throws FileNotFoundException {
+    // Arrange
+    String day10Part1Input =
+        "src/main/java/org/jmitchell238/aoc/aoc2023/day10/input_test_2.txt";
+    Day10 day10 = new Day10();
+
+    // Assert
+    long expected = 8L;
+    long actual = day10.part1(day10Part1Input);
+
+    // Act
+    assertThat(actual).isEqualTo(expected);
+  }
+
+  @Test
+  void Day10Part1_CalledWithRealInput_Expect_() throws FileNotFoundException {
+    // Arrange
+    String day10Part1Input = "src/main/java/org/jmitchell238/aoc/aoc2023/day10/input.txt";
+    Day10 day10 = new Day10();
+
+    // Assert
+    long expected = 0L;
+    long actual = day10.part1(day10Part1Input);
+
+    // Act
+    assertThat(actual).isEqualTo(expected);
+  }
+
+  @Test
+  void Day10Part2_CalledWithTestInput_Expect_() throws FileNotFoundException {
+    // Arrange
+    String day10Part2Input =
+        "src/main/java/org/jmitchell238/aoc/aoc2023/day10/input_test_1.txt";
+    Day10 day10 = new Day10();
+    day10.setIsPartTwo(true);
+
+    // Act
+    long expected = 2L;
+    long actual = day10.part2(day10Part2Input);
+
+    // Assert
+    assertThat(actual).isEqualTo(expected);
+  }
+
+  @Test
+  void Day10Part2_CalledWithRealInput_Expect_() throws FileNotFoundException {
+    // Arrange
+    String day10Part2Input = "src/main/java/org/jmitchell238/aoc/aoc2023/day10/input.txt";
+    Day10 day10 = new Day10();
+    day10.setIsPartTwo(true);
+
+    // Act
+    long expected = 0L;
+    long actual = day10.part2(day10Part2Input);
+
+    // Assert
+    assertThat(actual).isEqualTo(expected);
+  }
+}
