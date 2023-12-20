eef0e2ab57b74e77471d67505dda1876483ed9e8
James Mitchell <jmitchell238@gmail.com>
2023-12-20 15:02:11 -0600

    Setup For Java Day 12 AOC2023

---
+++ Java/AdventOfCodeJava/src/main/java/org/jmitchell238/aoc/aoc2023/day12/Day12.java
@@ -0,0 +1,57 @@
+package org.jmitchell238.aoc.aoc2023.day12;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import lombok.Getter;
+import lombok.Setter;
+
+public class Day12 {
+  private static final Boolean DEBUGGING = false;
+  @Getter @Setter
+  private Boolean isPartTwo = false;
+
+
+
+  public void main(String[] args) throws FileNotFoundException {
+    Day12Run();
+  }
+
+  public void Day12Run() throws FileNotFoundException {
+    System.out.println("\n--- Day 9: Mirage Maintenance ---\n");
+
+    String input = "src/main/java/org/jmitchell238/aoc/aoc2023/day12/input.txt";
+    String input_test = "src/main/java/org/jmitchell238/aoc/aoc2023/day12/input_test.txt";
+
+    long partOneAnswer = part1(input_test);
+    System.out.println(STR."Part 1: Answer: \{partOneAnswer}");
+
+    long partTwoAnswer = part2(input_test);
+    System.out.println(STR."Part 2: Answer: \{partTwoAnswer}");
+  }
+
+  public long part1(String filePath) throws FileNotFoundException {
+    processFile(filePath);
+
+    return -1L;
+  }
+
+  public long part2(String filePath) throws FileNotFoundException {
+    processFile(filePath);
+
+    return -1L;
+  }
+
+  private void processFile(String filePath) throws FileNotFoundException {
+    Scanner scanner = new Scanner(new File(filePath));
+
+    while (scanner.hasNextLine()) {
+      String line = scanner.nextLine();
+      if (DEBUGGING) {
+        System.out.println(line);
+      }
+    }
+
+    scanner.close();
+  }
+}
